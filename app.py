aad92dec08a5472f8ade546dd19cc13370a26899
dashengwzy <al.fayeed.weizy@gmail.com>
2019-04-14 18:11:51 +0800

    登陆页面功能完成，但是容错性还不够好，相关提示不够到位

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 
 # 导入 bs4 库,创建 Beautiful Soup 对象,用来从html文本中提取文本内容
 from bs4 import BeautifulSoup
-from flask import Flask, render_template, request, redirect, url_for, abort, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, abort, send_from_directory, session, g
 from flask_bootstrap import Bootstrap
 from flask_uploads import UploadSet, configure_uploads
 # 引入Python中的jquery PyQuery库
@@ -25,8 +25,10 @@ from models import User
 
 app = Flask(__name__)
 # 配置文件上传的路径以及限制条件
-app.config['UPLOADED_PHOTO_DEST'] =  os.path.join(os.path.dirname(os.path.abspath(__file__)), "static\images")
+app.config['UPLOADED_PHOTO_DEST'] = os.path.join(os.path.dirname(os.path.abspath(__file__)), "static\images")
 app.config['UPLOADED_PHOTO_ALLOW'] = ['png', 'jpg']
+# 设置SECRET_KEY以使用session
+app.config.update(SECRET_KEY='123456')
 # 实例化 UploadSet 对象
 photos = UploadSet('PHOTO')
 # 将 app 的 config 配置注册到 UploadSet 实例 photos
@@ -47,6 +49,7 @@ def upload():
         filename = photos.save(request.files['photo'])
         return redirect(url_for('show', name=filename))
     return render_template('upload.html')
+
 
 @app.route('/photo/<name>')
 def show(name):
@@ -136,9 +139,9 @@ def marine_organism(page, state):
         # search和分页不能同时实现的原因在于，search第二次分页因为走得是第一条路，所以数据不一样了。先不考虑直接点击按钮的问题
         key_word = request.form.get('organism_key_word')
         if key_word is None or key_word is "":
-                key_word = myglobal.get_value()
-                # 将关键字拼接成模糊字段
-                args = '%' + key_word + '%'
+            key_word = myglobal.get_value()
+            # 将关键字拼接成模糊字段
+            args = '%' + key_word + '%'
         else:
             myglobal.set_value(key_word)
             # 将关键字拼接成模糊字段
@@ -173,7 +176,8 @@ def organism_one(marine_organism_id):
         # 将关键字拼接成模糊字段
         args = '%' + key_word + '%'
         marine_organism_one = Marine_organism.query.filter(Marine_organism.id == marine_organism_id).first()
-        organism_datas = Organism_data.query.filter(Organism_data.data_name.like(args), Organism_data.data_kind == marine_organism_one.data_set_name).all()
+        organism_datas = Organism_data.query.filter(Organism_data.data_name.like(args),
+                                                    Organism_data.data_kind == marine_organism_one.data_set_name).all()
         context = {
             'marine_organism_one': marine_organism_one,
             'organism_datas': organism_datas
@@ -184,10 +188,10 @@ def organism_one(marine_organism_id):
 # 如果点击的是下载按钮，则进行文件下载
 @app.route('/download/file_name:<filename>', methods=['GET', 'POST'])
 def download(filename):
-        # print(filename)
-        if os.path.isfile(os.path.join('static/upload_file', filename)):
-            return send_from_directory('static/upload_file', filename, as_attachment=True)
-        abort(404)
+    # print(filename)
+    if os.path.isfile(os.path.join('static/upload_file', filename)):
+        return send_from_directory('static/upload_file', filename, as_attachment=True)
+    abort(404)
 
 
 # 海洋水文数据集总展示页面
@@ -206,9 +210,9 @@ def marine_hydrology(page, state):
         # search和分页不能同时实现的原因在于，search第二次分页因为走得是第一条路，所以数据不一样了。先不考虑直接点击按钮的问题
         key_word = request.form.get('hydrology_key_word')
         if key_word is None or key_word is "":
-                key_word = myglobal.get_value()
-                # 将关键字拼接成模糊字段
-                args = '%' + key_word + '%'
+            key_word = myglobal.get_value()
+            # 将关键字拼接成模糊字段
+            args = '%' + key_word + '%'
         else:
             myglobal.set_value(key_word)
             # 将关键字拼接成模糊字段
@@ -232,7 +236,7 @@ def hydrology_one(marine_hydrology_id):
         # 根据数据集的归属类型，查询到所有属于本数据集的所有数据
         # print(marine_hydrology_one.data_set_name)
         hydrology_datas = Hydrology_data.query.filter(
-        Hydrology_data.data_kind == marine_hydrology_one.data_set_name).all()
+            Hydrology_data.data_kind == marine_hydrology_one.data_set_name).all()
         context = {
             'marine_hydrology_one': marine_hydrology_one,
             'hydrology_datas': hydrology_datas
@@ -247,7 +251,8 @@ def hydrology_one(marine_hydrology_id):
         # 将关键字拼接成模糊字段
         args = '%' + hy_key_word + '%'
         marine_hydrology_one = Marine_hydrology.query.filter(Marine_hydrology.id == marine_hydrology_id).first()
-        hydrology_datas = Hydrology_data.query.filter(Hydrology_data.data_name.like(args), Hydrology_data.data_kind == marine_hydrology_one.data_set_name).all()
+        hydrology_datas = Hydrology_data.query.filter(Hydrology_data.data_name.like(args),
+                                                      Hydrology_data.data_kind == marine_hydrology_one.data_set_name).all()
         context = {
             'marine_hydrology_one': marine_hydrology_one,
             'hydrology_datas': hydrology_datas
@@ -271,9 +276,9 @@ def marine_chemistry(page, state):
         # search和分页不能同时实现的原因在于，search第二次分页因为走得是第一条路，所以数据不一样了。先不考虑直接点击按钮的问题
         key_word = request.form.get('chdrology_key_word')
         if key_word is None or key_word is "":
-                key_word = myglobal.get_value()
-                # 将关键字拼接成模糊字段
-                args = '%' + key_word + '%'
+            key_word = myglobal.get_value()
+            # 将关键字拼接成模糊字段
+            args = '%' + key_word + '%'
         else:
             myglobal.set_value(key_word)
             # 将关键字拼接成模糊字段
@@ -311,7 +316,8 @@ def chemistry_one(marine_chemistry_id):
         # 将关键字拼接成模糊字段
         args = '%' + ch_key_word + '%'
         marine_chemistry_one = Marine_chemistry.query.filter(Marine_chemistry.id == marine_chemistry_id).first()
-        chemistry_datas = Chemistry_data.query.filter(Chemistry_data.data_name.like(args), Chemistry_data.data_kind == marine_chemistry_one.data_set_name).all()
+        chemistry_datas = Chemistry_data.query.filter(Chemistry_data.data_name.like(args),
+                                                      Chemistry_data.data_kind == marine_chemistry_one.data_set_name).all()
         context = {
             'marine_chemistry_one': marine_chemistry_one,
             'chemistry_datas': chemistry_datas
@@ -322,7 +328,6 @@ def chemistry_one(marine_chemistry_id):
 # 数据服务展示页面
 @app.route('/data', methods=['GET', 'POST'])
 def data():
-
     return render_template('data.html')
 
 
@@ -338,12 +343,12 @@ def article_all(page, state):
     # print(articles.items[0].content)
     # 对文章正文用BeautifulSoup进行文本提取，抽取文本内容重新赋值，以便前台展示
     for article in articles.items:
-            html_doc = article.content
-            soup = BeautifulSoup(html_doc, 'html.parser')
-            for string in soup.stripped_strings:
-                article.content = repr(string)[0:170]
-                print(article.content)
-                # article.content = soup.span.string
+        html_doc = article.content
+        soup = BeautifulSoup(html_doc, 'html.parser')
+        for string in soup.stripped_strings:
+            article.content = repr(string)[0:170]
+            print(article.content)
+            # article.content = soup.span.string
     if request.method == 'GET' and state == 0:
         if page is None:
             page = 1
@@ -357,9 +362,9 @@ def article_all(page, state):
         # search和分页不能同时实现的原因在于，search第二次分页因为走得是第一条路，所以数据不一样了。先不考虑直接点击按钮的问题
         key_word = request.form.get('article_key_word')
         if key_word is None or key_word is "":
-                key_word = myglobal.get_value()
-                # 将关键字拼接成模糊字段
-                args = '%' + key_word + '%'
+            key_word = myglobal.get_value()
+            # 将关键字拼接成模糊字段
+            args = '%' + key_word + '%'
         else:
             myglobal.set_value(key_word)
             # 将关键字拼接成模糊字段
@@ -385,7 +390,7 @@ def article_one(article_id):
     return render_template('article_one.html', **context)
 
 
-# 登陆页面
+# 注册页面
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     # 如果是正常的加载当前页面
@@ -405,10 +410,10 @@ def register():
             # 增加数据
             # 增加：
             user1 = User(
-                             organ=organ,
-                             username=username,
-                             password=password,
-                            )
+                organ=organ,
+                username=username,
+                password=password,
+            )
             db.session.add(user1)
             # 事务
             db.session.commit()
@@ -428,7 +433,38 @@ def register():
 # 登陆页面
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    # 如果是正常的加载当前页面
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = User.query.filter(User.username == username).first()
+        if user and user.password == password:
+            print(user.id)
+            session['user_id'] = user.id
+            # 如果想在31天内都不需要登录
+            session.permanent = True
+            # # 修改顶部栏的内容
+            # path = 'E:/test1/templates/base.html'
+            # htmlfile = open(path, 'r', encoding='utf-8')
+            # htmlhandle = htmlfile.read()
+            # soup = BeautifulSoup(htmlhandle, 'lxml')
+            # login_change = soup.find_all(id='login_change')
+            # print(login_change[0])
+            # del login_change[0]['id']
+            # login_change[0].string = "New link text."
+            # print(login_change)
+            return redirect(url_for('index'))
+        else:
+            return render_template('login.html')
+
+# 注销登录
+@app.route('/logout/')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
+
 
 
 # 装饰函数，运行在最前面
@@ -436,7 +472,19 @@ def login():
 def before_request():
     # 初始化全局变量
     myglobal.set_value("")
+    user_id = session.get('user_id')
+    if user_id:
+        user = User.query.filter(User.id == user_id).first()
+        if user:
+            g.user = user
 
 
-if __name__=='__main__':
+@app.context_processor
+def my_context_processor():
+    if hasattr(g, 'user'):
+        return {'user': g.user}
+    return {}
+
+
+if __name__ == '__main__':
     app.run()
